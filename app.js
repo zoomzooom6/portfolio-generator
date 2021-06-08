1abe004df723f42f504ba58abe901e0ec7e329f5
zoomzoom6 <tonyhuang107@live.ca>
2021-06-08 15:36:16 -0400

    create html file using fs module

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if(err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+})
+
+//Section 9.1
+/*const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const printProfileData = profileDataArr => {
     profileDataArr.forEach((profileItem) => console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs); */
