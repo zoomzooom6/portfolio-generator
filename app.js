d0c20e11816435d3f4204097a898932294fef5e0
zoomzoom6 <tonyhuang107@live.ca>
2021-06-08 16:10:08 -0400

    setup inquirer npm

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if(err) throw err;
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-})
+// fs.writeFile('index.html', pageHTML, err => {
+//     if(err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// })
 
 //Section 9.1
-/*const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+// const printProfileData = profileDataArr => {
+//     profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs); */
+// printProfileData(profileDataArgs); 
