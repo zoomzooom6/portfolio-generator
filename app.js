194cb90797dfb76fc998a41580a69e9a40363e29
zoomzoom6 <tonyhuang107@live.ca>
2021-06-08 22:44:45 -0400

    create more of HTML file to include projects

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js')
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
 
 // const pageHTML = generatePage(name, github);
 
@@ -134,8 +134,12 @@ const promptProject = portfolioData => {
 
 promptUser()
     .then(promptProject)
-    .then(projectAnswers => {
-        console.log(projectAnswers)
+    .then(portfolioData => {
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+        })
     });
 // fs.writeFile('index.html', pageHTML, err => {
 //     if(err) throw err;
