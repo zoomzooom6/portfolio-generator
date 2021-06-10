1354611a74b369929c48e148a9cfde9165debc24
zoomzoom6 <tonyhuang107@live.ca>
2021-06-09 22:02:23 -0400

    simplify generateProjects function with functions

--- src/page-template.js
+++ src/page-template.js
@@ -15,58 +15,43 @@ const generateAbout = aboutText => {
 //create project sections
 const generateProjects = projectsArr => {
 
-    //get array of just featured projects
-    const featureProjects = projectsArr.filter(project => {
-        if (project.feature) {
-            return true;
-        } else {
-            return false;
-        }
-    });
-
-    //get array of all non-featured projects
-    const nonFeaturedProjects = projectsArr.filter(project => {
-        if (!project.feature) {
-            return true;
-        } else {
-            return false;
-        }
-    })
-
-    const featuredProjectHtmlArr = featureProjects.map(({name, description, languages, link}) => {
-        return `
-            <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
-                <h3 class="portfolio-item-title text-light">${name}</h3>
-                <h5 class="portfolio-languages">
-                    Built With:
-                    ${languages.join(', ')}
-                </h5>
-                <p>${description}</p>
-                <a href="${link}" class="btn mt-auto" target="_blank"><i class="fab fa-github mr-2"></i>View Project on GitHub</a> 
-            </div>
-        `;
-    });
-
-    const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(({name, description, languages, link}) => {
-        return `
-            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-                <h3 class="portfolio-item-title text-light">${name}</h3>
-                <h5 class="portfolio-languages">
-                    Built With:
-                    ${languages.join(', ')}
-                </h5>
-                <p>${description}</p>
-                <a href="${link}" class="btn mt-auto" target="_blank"><i class="fab fa-github mr-2"></i>View Project on GitHub</a> 
-            </div>
-        `;
-    });
-
     return `
         <section class="my-3" id="portfolio">
             <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
             <div class="flex-row justify-space-between">
-                ${featuredProjectHtmlArr.join(', ')}
-                ${nonFeaturedProjectHtmlArr.join(', ')}
+            ${projectsArr
+            .filter(({ feature }) => feature)
+            .map(({ name, description, languages, link }) => {
+                return `
+                    <div class="col-12 mb-2 bg-dark text-light p-3">
+                        <h3 class="portfolio-item-title text-light">${name}</h3>
+                        <h5 class="portfolio-languages">
+                            Built With:
+                            ${languages.join(', ')}
+                        </h5>
+                        <p>${description}</p>
+                        <a href="${link}" class="btn" target="_blank"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+                    </div>
+                    `;
+            })
+            .join('')}
+                    
+            ${projectsArr
+            .filter(({ feature }) => !feature)
+            .map(({ name, description, languages, link }) => {
+                return `
+                    <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+                        <h3 class="portfolio-item-title text-light">${name}</h3>
+                        <h5 class="portfolio-languages">
+                            Built With:
+                            ${languages.join(', ')}
+                        </h5>
+                        <p>${description}</p>
+                        <a href="${link}" class="btn mt-auto" target="_blank"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+                    </div>
+                `;
+            })
+            .join('')}
             </div>
         </section>
     `;
@@ -111,6 +96,64 @@ module.exports = templateData => {
     </html>
     `;
 };
+
+//const generateProjects = projectsArr => { 
+// get array of just featured projects
+// const featureProjects = projectsArr.filter(project => {
+//     if (project.feature) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// });
+
+// //get array of all non-featured projects
+// const nonFeaturedProjects = projectsArr.filter(project => {
+//     if (!project.feature) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// })
+
+// const featuredProjectHtmlArr = featureProjects.map(({ name, description, languages, link }) => {
+//     return `
+//             <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
+//                 <h3 class="portfolio-item-title text-light">${name}</h3>
+//                 <h5 class="portfolio-languages">
+//                     Built With:
+//                     ${languages.join(', ')}
+//                 </h5>
+//                 <p>${description}</p>
+//                 <a href="${link}" class="btn mt-auto" target="_blank"><i class="fab fa-github mr-2"></i>View Project on GitHub</a> 
+//             </div>
+//         `;
+// });
+
+// const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(({ name, description, languages, link }) => {
+//     return `
+//             <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+//                 <h3 class="portfolio-item-title text-light">${name}</h3>
+//                 <h5 class="portfolio-languages">
+//                     Built With:
+//                     ${languages.join(', ')}
+//                 </h5>
+//                 <p>${description}</p>
+//                 <a href="${link}" class="btn mt-auto" target="_blank"><i class="fab fa-github mr-2"></i>View Project on GitHub</a> 
+//             </div>
+//         `;
+// });
+
+// return `
+//         <section class="my-3" id="portfolio">
+//             <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+//             <div class="flex-row justify-space-between">
+//                 ${featuredProjectHtmlArr.join(', ')}
+//                 ${nonFeaturedProjectHtmlArr.join(', ')}
+//             </div>
+//         </section>
+//     `;
+// };
 
 // const generatePage = (userName, githubName) => {
 //     return `
